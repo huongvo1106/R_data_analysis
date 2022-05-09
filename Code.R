71342a130097e9c18c8a8a2b76f0842da6ebf7c9
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2022-05-09 09:22:30 -0500

    Add files via upload

--- Code.R
+++ Code.R
@@ -4,14 +4,19 @@ x
 getwd()
 setwd("/Users/huongphucquynhvo/InnovationX/Bias_AI")
 file <- read.csv("data.csv")
-file_clean <- file[!(file$PhFemalePitchMean%in% c("444","999","9999999")),]
+file_clean <- file[!(file$CCSexual%in% c("444","999","9999999")),]
+total <- nrow(file_clean)
+print(total)
 x <- "Hispanic***********************"
 x
 asian <- file_clean
 asian$Race[asian$Race != 8] <- 0
 asian$Race[asian$Race == 8] <- 1
-
-summary(lme(PhFemalePitchMean ~ Race, 
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
             random = ~ Time | ID/ID_index, 
             data=asian))
 
@@ -20,10 +25,16 @@ x
 asian <- file_clean
 asian$Race[asian$Race != 7] <- 0
 asian$Race[asian$Race == 7] <- 1
-
-summary(lme(PhFemalePitchMean ~ Race, 
-            random = ~ Time | ID/ID_index, 
+print(length(which(asian$Race == 0)))
+print(length(which(asian$Race == 1)))
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
+            random = ~ Time | ID/ID_index,
             data=asian))
+
 
 
 x <- "Multi-racial**************************"
@@ -31,9 +42,13 @@ x
 asian <- file_clean
 asian$Race[asian$Race != 6] <- 0
 asian$Race[asian$Race == 6] <- 1
-
-
-summary(lme(PhFemalePitchMean ~ Race, 
+print(length(which(asian$Race == 0)))
+print(length(which(asian$Race == 1)))
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
             random = ~ Time | ID/ID_index, 
             data=asian))
 
@@ -42,9 +57,13 @@ x
 asian <- file_clean
 asian$Race[asian$Race != 5] <- 0
 asian$Race[asian$Race == 5] <- 1
-
-
-summary(lme(PhFemalePitchMean ~ Race, 
+print(length(which(asian$Race == 0)))
+print(length(which(asian$Race == 1)))
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
             random = ~ Time | ID/ID_index, 
             data=asian))
 
@@ -53,9 +72,13 @@ x
 asian <- file_clean
 asian$Race[asian$Race != 4] <- 0
 asian$Race[asian$Race == 4] <- 1
-
-
-summary(lme(PhFemalePitchMean ~ Race, 
+print(length(which(asian$Race == 0)))
+print(length(which(asian$Race == 1)))
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
             random = ~ Time | ID/ID_index, 
             data=asian))
 
@@ -64,9 +87,13 @@ x
 asian <- file_clean
 asian$Race[asian$Race != 2] <- 0
 asian$Race[asian$Race == 2] <- 1
-
-
-summary(lme(PhFemalePitchMean ~ Race, 
+print(length(which(asian$Race == 0)))
+print(length(which(asian$Race == 1)))
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
             random = ~ Time | ID/ID_index, 
             data=asian))
 
@@ -75,8 +102,12 @@ x
 asian <- file_clean
 asian$Race[asian$Race != 3] <- 0
 asian$Race[asian$Race == 3] <- 1
-
-
-summary(lme(PhFemalePitchMean ~ Race, 
+print(length(which(asian$Race == 0)))
+print(length(which(asian$Race == 1)))
+race_total = sum(asian$Race == 1)
+print(race_total)
+print("Percent")
+print(race_total / total)
+summary(lme(CCSexual ~ Race, 
             random = ~ Time | ID/ID_index, 
             data=asian))
